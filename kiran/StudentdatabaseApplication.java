8bbc11ca6752ea89ae4df8c0ac4a94b81bc660c9
kiran2026git <145956141+kiran2026git@users.noreply.github.com>
2023-09-26 21:33:45 +0530

    Add files via upload

---
+++ kiran/StudentdatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.kiran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentdatabaseApplication.class, args);
+	}
+
+}
+
